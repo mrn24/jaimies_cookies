d655e13870be74fba55a0ca73204a6020fd169b6
Matt <mrn24@nau.edu>
2020-06-22 21:13:46 -0400

    initial commit, begin shopping cart

---
+++ src/app/services/cart.service.ts
@@ -0,0 +1,75 @@
+import { Injectable } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
+
+export interface Product{
+  id: number;
+  name: string;
+  price: number;
+  amount: number;
+}
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CartService {
+  data: Product [] = [
+    {id: 0, name:'Gingerbread Cookie', price: 8.99, amount: 0},
+    {id: 1, name:'Sugar Cookie', price: 4.99, amount: 0},
+    {id: 2, name:'Thin Mint Cookie', price: 7.99, amount: 0},
+    {id: 3, name:'Super Awesome Cookie', price: 9.99, amount: 0},
+  ];
+
+  private cart = [];
+  private cartItemCount = new BehaviorSubject(0);
+
+  constructor() { }
+
+  getProducts() {
+    return this.data;
+  }
+
+  getCart() {
+    return this.cart;
+  }
+
+  getCartItemCount() {
+    return this.cartItemCount;
+  }
+
+  addProduct(product){
+    let added = false;
+    for (let p of this.cart) {
+      if (p.id === product.id) {
+        p.amount += 1;
+        added = true;
+        break;
+      }
+    }
+    if (!added){
+      product.amount = 1;
+      this.cart.push(product);
+    }
+    this.cartItemCount.next(this.cartItemCount.value + 1);
+  }
+
+  decreaseProduct(product){
+    for (let [index, p] of this.cart.entries()) {
+      if (p.id === product.id) {
+        p.amount -= 1;
+        if (p.amount == 0) {
+          this.cart.splice(index, 1);
+        }
+      }
+    }
+    this.cartItemCount.next(this.cartItemCount.value - 1);
+  }
+
+  removeProduct(product) {
+    for (let [index, p] of this.cart.entries()) {
+      if (p.id === product.id) {
+        this.cartItemCount.next(this.cartItemCount.value - p.amount);
+        this.cart.splice(index, 1);
+      }
+    }
+  }
+}
